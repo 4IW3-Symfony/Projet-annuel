6009dd69baf8b4ec128b1a7ce40751e6a43cabf1
pthushanth <pthushanth97@gmail.com>
2022-04-03 00:29:40 +0200

    Add new feature to update user profile

--- src/Controller/Dashboard/DefaultController.php
+++ src/Controller/Dashboard/DefaultController.php
@@ -2,12 +2,13 @@
 
 namespace App\Controller\Dashboard;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use App\Form\UserType;
+use App\Form\ProfileType;
 use Symfony\Component\Security\Core\Security;
 
 class DefaultController extends AbstractController
@@ -33,14 +34,18 @@ class DefaultController extends AbstractController
     #[Route('dashboard/profile', name: 'dashboard_profile', methods: ['GET', 'POST'])]
     public function profile(Request $request, EntityManagerInterface $entityManager): Response
     {
+
         $user = $this->security->getUser();
-        $form = $this->createForm(UserType::class, $user, ['method' => 'PATCH']);
-        // $form = $this->createForm(UserType::class, $user);
-        // $form->handleRequest($request);
-        //  $form->handleRequest($request,false);
-        $form->submit($request->request->get($form->getName()), false);
+        // $userId = $user->getId();
+        // dd($user);
+        // $user = $entityManager->getRepository(User::class)->find($userId);
+        $form = $this->createForm(ProfileType::class, $user);
+
+        $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            /** @var \App\Entity\User $user */
             $entityManager->flush();
+            $this->addFlash('success', 'Profile updated successfully');
             return $this->redirectToRoute('dashboard_profile', [], Response::HTTP_SEE_OTHER);
         }
 
