8cf8f0c02f7df1c3907399be2a41feb06b2d1284
Seb <sebastien.gaudard.1@gmail.com>
2022-04-04 06:29:41 +0200

    :sparkles: Twig backoffice base template

---
+++ frontend/PADashboard/vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.25.0',
+      'version' => '1.25.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '30885182c981ab175d4d034db0f6f469898070ab',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.25.0',
+      'version' => '1.25.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0abb51d2f102e00a4eefcf46ba7fec406d245825',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v3.3.9',
+      'version' => '3.3.9.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6ff9b0e440fa66f97f207e181c41340ddfa5683d',
+    ),
+  ),
+);
