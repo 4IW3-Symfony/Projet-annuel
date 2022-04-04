adb2de7d1b4675e760a6dc3cb69a178365b6ace4
pthushanth <pthushanth97@gmail.com>
2022-04-04 23:21:40 +0200

    Add Password field and show user avatar in the header

--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -8,6 +8,10 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProfileType extends AbstractType
@@ -23,8 +27,32 @@ class ProfileType extends AbstractType
                 'delete_label' => 'Remove Image',
                 'allow_delete' => true,
             ])
-            // ->add('password')
-            ->add('firstname')
+            ->add('password', RepeatedType::class, array(
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => array(
+                    'attr' => array(
+                        'class' => 'form-control'
+                    )
+                ),
+                'required' => true,
+                'first_options'  => array('label' => 'Password'),
+                'second_options' => array('label' => 'Repeat Password'),
+            ))
+            ->add('firstname', null, [
+                'label_attr' => ["class" => "col-12"],
+            ])
             ->add('lastname')
             ->add('dateOfBirth', DateType::class, [
                 'label' => 'Date of birth'
@@ -39,7 +67,7 @@ class ProfileType extends AbstractType
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-                'data_class' => User::class,
+            'data_class' => User::class,
         ]);
     }
 }
