b3ed73f93dba139bd9fb10b4db91607331c68422
Chaochao <chaochao.zhou@hotmail.com>
2022-07-04 16:47:57 +0200

    Add condition form rentalReservationType

--- src/Form/RetalReservationType.php
+++ src/Form/RetalReservationType.php
@@ -2,19 +2,32 @@
 
 namespace App\Form;
 
+use DateTime;
 use App\Entity\Rental;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Validator\Constraints;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class RetalReservationType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('date_start')
-            ->add('date_end')
+            ->add('date_start',DateType::class,[
+                'widget' => 'single_text',
+                'attr' => [
+                    'min' => (new DateTime('now'))->format('Y-m-d'),
+                ],
+            ])
+            ->add('date_end', DateType::class,[
+                'widget' => 'single_text',
+                'attr' => [
+                    'min' => (new DateTime('now'))->format('Y-m-d'),
+                ],
+            ])
         ;
     }
 
